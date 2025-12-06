985ecde370effeaa1d4103acb39da2bda908ba6a
Ilyas Doughmi <doughmi.projects@gmail.com>
2025-12-06 22:34:56 +0100

    fix: fixing problem in path

--- includes/login.php
+++ includes/login.php
@@ -4,8 +4,8 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     $email = $_POST["email"];
     $password = $_POST["password"];
 
-    require_once("./Classes/Login.php");
-    require_once("./php/connect.php");
+    require_once("../Classes/Login.php");
+    require_once("../php/connect.php");
 
     $login = new Login($email,$password);
 }
