b4127daf0e18af7460c0f5395f7cbfa7889efc1a
Ilyas Doughmi <doughmi.projects@gmail.com>
2025-12-06 23:27:16 +0100

    feat: adding if connected to redirect auto to dashboard

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<?php
+    session_start();
+    
+    if(isset($_SESSION["id"])){
+        header("location: pages/dashboard.php");
+        exit;
+    }
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
