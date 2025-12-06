7a548980f6815403c55ea0551da120cf8dc185cd
Ilyas Doughmi <doughmi.projects@gmail.com>
2025-12-06 23:27:42 +0100

    feat: adding new class for project

---
+++ Classes/Project.php
@@ -0,0 +1,10 @@
+<?php
+Class Project extends db{
+    private $project_id;
+    private $user_id;
+    private $project_name;
+    private $port;
+    private $container_name;
+    private $status;
+}
+
