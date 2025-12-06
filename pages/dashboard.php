dd95b5a97e6d08f6f2e7a64c4a9f31c9d95321c1
Ilyas Doughmi <doughmi.projects@gmail.com>
2025-12-06 23:51:52 +0100

    feat: creating new object instance and making new variable to get all user projects

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -5,5 +5,10 @@
         header("location: ../index.php");
         exit;
     }
+    require_once("../php/connect.php");
+    require_once("../Classes/Project.php");
+
+    $projects = new Project();
+    $user_Projects = $projects->getProjects($_SESSION["id"]);
 
 ?>
