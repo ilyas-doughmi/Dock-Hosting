dc352f7c760a09d3cecd8e41e130e68d3367a9fb
Ilyas Doughmi <doughmi.projects@gmail.com>
2025-12-07 10:34:36 +0100

    feat: separate path variables to 2 so we can have file director and path of the project

--- includes/create-project.php
+++ includes/create-project.php
@@ -3,13 +3,14 @@
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     $project_name = $_POST["project_name"];
     $file = $_FILES["files"];
-
     $path = "C:/xampp/htdocs/Dock-Hosting/users/Projects/". $_SESSION["id"]."/".$project_name."/";
 
     if(!is_dir($path)){
         mkdir($path,0777,true);
     }
 
-    $path = $path . $file["name"];
-    move_uploaded_file($file["tmp_name"],$path);
+    $file_dir = $path . $file["name"];
+    move_uploaded_file($file["tmp_name"],$file_dir);
+
+
 }
