fd5289b1e593f8d615bc44054c47d1a67328a308
Ilyas Doughmi <doughmi.projects@gmail.com>
2025-12-07 09:52:59 +0100

    feat: adding to creating a foulder for every user so we avoid collisions in files names

--- includes/create-project.php
+++ includes/create-project.php
@@ -1,10 +1,14 @@
 <?php 
-
+    session_start();
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     $project_name = $_POST["project_name"];
     $file = $_FILES["files"];
-    
-    $path = "C:/xampp/htdocs/Dock-Hosting/users/Projects/";
+
+    $path = "C:/xampp/htdocs/Dock-Hosting/users/Projects/". $_SESSION["id"]."/";
+
+    if(!is_dir($path)){
+        mkdir($path,true);
+    }
 
     $path = $path . $file["name"];
     move_uploaded_file($file["tmp_name"],$path);
