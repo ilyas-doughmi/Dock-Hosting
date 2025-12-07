caf4135e667801852ae17c2d5ac7180b67c94902
Ilyas Doughmi <doughmi.projects@gmail.com>
2025-12-07 11:43:19 +0100

    feat: adding new object from project class and adding to find if we have a port it will increment by 1 if not it will set the default to port 8000

--- includes/create-project.php
+++ includes/create-project.php
@@ -1,23 +1,41 @@
-<?php 
-    session_start();
-if($_SERVER["REQUEST_METHOD"] == "POST"){
+<?php
+session_start();
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    require_once "../php/connect.php";
+    require_once "../Classes/Project.php";
+
     $project_name = $_POST["project_name"];
     $file = $_FILES["files"];
-    $path = "C:/xampp/htdocs/Dock-Hosting/users/Projects/". $_SESSION["id"]."/".$project_name."/";
+    $path = "C:/xampp/htdocs/Dock-Hosting/users/Projects/" . $_SESSION["id"] . "/" . $project_name . "/";
 
-    if(!is_dir($path)){
-        mkdir($path,0777,true);
+    if (!is_dir($path)) {
+        mkdir($path, 0777, true);
     }
 
     $file_dir = $path . $file["name"];
-    move_uploaded_file($file["tmp_name"],$file_dir);
+    move_uploaded_file($file["tmp_name"], $file_dir);
 
-        // extract file from zip
+    // extract file from zip
 
     $extract = new ZipArchive;
-    if($extract->open($file_dir) === TRUE){
+    if ($extract->open($file_dir) === TRUE) {
         $extract->extractTo($path);
         $extract->close();
-        echo "good :)";
     }
+
+    // free port
+
+    $Projects = New Project();
+    $last_port = $Projects->trackPort();
+
+    if ($last_port) {
+        echo "find already port incremenet by 1";
+        $last_port += 1;
+    } else {
+        echo "finding nothing";
+        $last_port = 8000;
+    }
+    
+    echo $last_port;
 }
