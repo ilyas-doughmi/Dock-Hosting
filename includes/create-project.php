886cdaf24d21561904b5d2dada575d99ec013a71
Ilyas Doughmi <doughmi.projects@gmail.com>
2025-12-07 09:34:25 +0100

    feat: adding to move file to new foulder to save the project

--- includes/create-project.php
+++ includes/create-project.php
@@ -3,4 +3,9 @@
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     $project_name = $_POST["project_name"];
     $file = $_FILES["files"];
+    
+    $path = "C:/xampp/htdocs/Dock-Hosting/users/Projects/";
+
+    $path = $path . $file["name"];
+    move_uploaded_file($file["tmp_name"],$path);
 }
