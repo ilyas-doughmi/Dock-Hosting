b1e09fb19cde4810212a09ac089a7268983b5cb1
Ilyas Doughmi <doughmi.projects@gmail.com>
2025-12-07 10:22:30 +0100

    fix: fixing missing param in mkdir adding permission to let anyone can edit excute and create

--- includes/create-project.php
+++ includes/create-project.php
@@ -4,10 +4,10 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     $project_name = $_POST["project_name"];
     $file = $_FILES["files"];
 
-    $path = "C:/xampp/htdocs/Dock-Hosting/users/Projects/". $_SESSION["id"]."/";
+    $path = "C:/xampp/htdocs/Dock-Hosting/users/Projects/". $_SESSION["id"]."/".$project_name."/";
 
     if(!is_dir($path)){
-        mkdir($path,true);
+        mkdir($path,0777,true);
     }
 
     $path = $path . $file["name"];
