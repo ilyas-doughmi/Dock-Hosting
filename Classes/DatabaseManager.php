bec6eb0f196c60ba8bcb4d56e17a3e8b1ed81609
Ilyas Doughmi <doughmi.projects@gmail.com>
2025-12-28 17:39:40 +0100

    feat: implement DatabaseManager class for database creation and retrieval

---
+++ Classes/DatabaseManager.php
@@ -0,0 +1,51 @@
+<?php
+
+class DatabaseManager extends db {
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function createDatabase($userId, $projectName) {
+        $suffix = substr(bin2hex(random_bytes(4)), 0, 6);
+        $cleanProjectName = preg_replace('/[^a-zA-Z0-9]/', '', $projectName);
+        
+        $dbName = "db_" . $userId . "_" . substr($cleanProjectName, 0, 10) . "_" . $suffix;
+        $dbUser = "u_" . $userId . "_" . $suffix;
+        $dbPass = bin2hex(random_bytes(12));
+
+        try {
+            $pdo = $this->connect();
+            
+            $sql = "
+                CREATE DATABASE `$dbName`;
+                CREATE USER '$dbUser'@'%' IDENTIFIED BY '$dbPass';
+                GRANT ALL PRIVILEGES ON `$dbName`.* TO '$dbUser'@'%';
+                FLUSH PRIVILEGES;
+            ";
+            $pdo->exec($sql);
+
+            $saveSql = "INSERT INTO user_databases (user_id, project_name, db_name, db_user, db_password) VALUES (:uid, :pname, :dbname, :dbuser, :dbpass)";
+            $stmt = $pdo->prepare($saveSql);
+            $stmt->execute([
+                ':uid' => $userId, 
+                ':pname' => $projectName,
+                ':dbname' => $dbName, 
+                ':dbuser' => $dbUser, 
+                ':dbpass' => $dbPass
+            ]);
+
+            return ['name' => $dbName, 'user' => $dbUser, 'pass' => $dbPass];
+
+        } catch (PDOException $e) {
+            return false;
+        }
+    }
+
+    public function getDatabase($userId, $projectName) {
+        $query = "SELECT * FROM user_databases WHERE user_id = :uid AND project_name = :pname LIMIT 1";
+        $stmt = $this->connect()->prepare($query);
+        $stmt->execute([':uid' => $userId, ':pname' => $projectName]);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+}
