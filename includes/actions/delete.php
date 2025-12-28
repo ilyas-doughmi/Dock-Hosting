7cc7388faa86516db89d8343951fb33eea6ec31a
Ilyas Doughmi <doughmi.projects@gmail.com>
2025-12-28 10:01:28 +0100

    fix: use dynamic path for project deletion to support VPS

--- includes/actions/delete.php
+++ includes/actions/delete.php
@@ -5,7 +5,8 @@ session_start();
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $container = $_POST["container_name"];
     $project_name = $_POST["container_name"];
-    $path = "C:/xampp/htdocs/Dock-Hosting/users/Projects/" . $_SESSION["id"] . "/" . $project_name . "/";
+    $base_path = dirname(__DIR__, 2);
+    $path = $base_path . "/users/Projects/" . $_SESSION["id"] . "/" . $project_name . "/";
 
     $project = new Project;
 
