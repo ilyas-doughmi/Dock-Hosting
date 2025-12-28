b928726679a180d28d920a975571de5fc55bdba4
Ilyas Doughmi <doughmi.projects@gmail.com>
2025-12-28 15:11:24 +0100

    fix: restore missing file/folder creation actions with dynamic paths

---
+++ includes/actions/create_folder.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+
+if (!isset($_SESSION["id"]) || $_SERVER["REQUEST_METHOD"] != "POST") {
+    header("location: ../../index.php");
+    exit;
+}
+
+$container_name = $_POST["container"];
+$path_rel = $_POST["path"];
+$folder_name = $_POST["name"];
+
+if (empty($container_name) || empty($folder_name)) {
+    header("location: ../../pages/dashboard.php");
+    exit;
+}
+
+$base_path = dirname(__DIR__, 2);
+$target_dir = $base_path . "/users/Projects/" . $_SESSION["id"] . "/" . $container_name . "/";
+
+if ($path_rel != "") {
+    $target_dir .= $path_rel . "/";
+}
+
+$full_path = $target_dir . $folder_name;
+
+if (strpos(realpath($target_dir), realpath($base_path . "/users/Projects/" . $_SESSION["id"])) !== 0) {
+    header("location: ../../pages/file-manager.php?container=" . $container_name . "&error=invalid_path");
+    exit;
+}
+
+if (!file_exists($full_path)) {
+    mkdir($full_path, 0777, true);
+}
+
+header("location: ../../pages/file-manager.php?container=" . $container_name . "&path=" . $path_rel);
+exit;
