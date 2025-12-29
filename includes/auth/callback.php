b9ab1665bb65d430b4ef5635d3b7e8c360494bd3
Ilyas Doughmi <doughmi.projects@gmail.com>
2025-12-29 23:02:27 +0100

    feat: Enhance GitHub OAuth callback handling with access token retrieval

--- includes/auth/callback.php
+++ includes/auth/callback.php
@@ -1,8 +1,35 @@
 <?php
 
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../../');
+$dotenv->load();
+
 
 if(isset($_GET["code"])){
-    echo "we have it " . $_GET["code"];
+    $code = $_GET["code"];
+
+    $payload = [
+        'client_id' => $_ENV['GITHUB_CLIENT_ID'],
+        'client_secret' => $_ENV['GITHUB_CLIENT_SECRET'],
+        'code' => $code
+    ];
+
+    $ch = curl_init("https://github.com/login/oauth/access_token");
+
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, ['Accept: application/json']);
+
+    $response = curl_exec($ch);
+    curl_close($ch);
+    $data = json_decode($response, true);
+    
+    echo "<pre>";
+    print_r($data);
+    echo "</pre>";
 }
 else{
     echo "problem";
