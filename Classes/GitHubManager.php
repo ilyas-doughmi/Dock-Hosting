e9b657de98640250c83d19082f40a953922eb0bc
Ilyas Doughmi <doughmi.projects@gmail.com>
2025-12-29 23:39:25 +0100

    feat: Implement getAccessToken method for retrieving OAuth tokens by user ID

--- Classes/GitHubManager.php
+++ Classes/GitHubManager.php
@@ -4,5 +4,20 @@ require_once __DIR__ . "/../php/connect.php";
 
 
 class GitHubManager{
-    
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = new db();
+    }
+    public function getAccessToken($user_id)
+    {
+        $query = "SELECT * FROM oauth_tokens WHERE user_id = :user_id";
+        $stmt = $this->db->connect()->prepare($query);
+        $stmt->execute([
+            ":user_id" => $user_id
+        ]);
+
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
 }
